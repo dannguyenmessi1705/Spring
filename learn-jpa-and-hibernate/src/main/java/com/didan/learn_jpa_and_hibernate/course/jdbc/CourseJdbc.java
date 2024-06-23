cf73fa07926a4879aeb4387db42fd16b70dfe5fd
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-23 15:48:49 +0700

    Jdbc: use with ? to pass value

--- learn-jpa-and-hibernate/src/main/java/com/didan/learn_jpa_and_hibernate/course/jdbc/CourseJdbc.java
+++ learn-jpa-and-hibernate/src/main/java/com/didan/learn_jpa_and_hibernate/course/jdbc/CourseJdbc.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.didan.learn_jpa_and_hibernate.course.Course;
+
 @Repository // Đánh dấu đây là một bean của Spring, dùng để thực hiện các thao tác với
 			// database
 public class CourseJdbc {
@@ -19,7 +21,33 @@ public class CourseJdbc {
 	// String trong câu lệnh SQL phải được viết trong dấu nháy đơn `'` thay vì dấu
 	// nháy kép `"`
 
+	// Ngoài ra với các tham số động, chúng ta có thể sử dụng cú pháp `?` để thay
+	// thế
+	public static String INSERT_TEMPLATE = """
+			INSERT INTO courses (id, name, author)
+			VALUES
+			(?, ?, ?);
+			""";
+
+	public static String DELETE_TEMPLATE = """
+			DELETE FROM courses WHERE id = ?;
+			""";
+
 	public void insert() {
 		jdbcTemplate.update(INSERT_SQL); // Thực thi câu lệnh SQL INSERT_SQL bằng jdbcTemplate
+	}
+
+	public void insertTemplate(Course course) {
+		jdbcTemplate.update(INSERT_TEMPLATE, course.getId(), course.getName(), course.getAuthor()); // Thực thi câu lệnh
+																									// SQL
+																									// INSERT_TEMPLATE
+																									// bằng jdbcTemplate
+																									// với các tham số
+																									// động
+	}
+
+	public void deleteId(long id) {
+		jdbcTemplate.update(DELETE_TEMPLATE, id); // Thực thi câu lệnh SQL DELETE_TEMPLATE bằng jdbcTemplate với các
+													// tham số động
 	}
 }
--- learn-jpa-and-hibernate/src/main/java/com/didan/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/didan/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.didan.learn_jpa_and_hibernate.course.Course;
+
 @Component // Đánh dấu đây là một bean của Spring, dùng để thực thi một số thao tác khi ứng
 			// dụng chạy
 public class CourseJdbcCommandLineRunner implements CommandLineRunner { // CommandLineRunner là một interface của Spring
@@ -16,6 +18,9 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner { // Comma
 	@Override
 	public void run(String... args) throws Exception {
 		courseJdbc.insert(); // Gọi phương thức insert() của bean CourseJdbc khi ứng dụng chạy
+		courseJdbc.insertTemplate(new Course(2, "NextJs", "didan"));
+		courseJdbc.insertTemplate(new Course(3, "Python", "didan"));
+		courseJdbc.deleteId(1);
 	}
 
 }
