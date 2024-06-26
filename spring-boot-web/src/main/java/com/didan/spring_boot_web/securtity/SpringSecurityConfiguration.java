e25044ee419667305a20014e9691bfe78a835084
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-26 15:00:26 +0700

    SpringSecurity: Config FilterChain for H2Database

--- spring-boot-web/src/main/java/com/didan/spring_boot_web/securtity/SpringSecurityConfiguration.java
+++ spring-boot-web/src/main/java/com/didan/spring_boot_web/securtity/SpringSecurityConfiguration.java
@@ -4,11 +4,15 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults; // Import thêm Customizer để sử dụng withDefaults trong HttpSecurity
 
 @Configuration // Đánh dấu đây là class cấu hình Spring (Bên trong có thể chứa các Bean)
 public class SpringSecurityConfiguration {
@@ -33,16 +37,31 @@ public class SpringSecurityConfiguration {
 
 		UserDetails userDetails1 = User.builder().passwordEncoder(passwordEncoderFunction).username("didannguyen")
 				.password("12345").roles("ADMIN", "USER").build(); // Tạo ra một đối tượng UserDetails với việc được mã
-																	// hóa mật khẩu, username là didannguyen, 
+																	// hóa mật khẩu, username là didannguyen,
 																	// password là 12345, và có 2 role là
 																	// ADMIN và USER
-		
+
 		UserDetails userDetails2 = User.builder().passwordEncoder(passwordEncoderFunction).username("lionelmessi")
 				.password("12345").roles("ADMIN", "USER").build(); // Tạo ra một đối tượng UserDetails với việc được mã
-																	// hóa mật khẩu, username là didannguyen, 
+																	// hóa mật khẩu, username là didannguyen,
 																	// password là 12345, và có 2 role là
 																	// ADMIN và USER
-		return new InMemoryUserDetailsManager(userDetails1, userDetails2); // Trả về một đối tượng InMemoryUserDetailsManager với
-															// user vừa tạo
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2); // Trả về một đối tượng
+																			// InMemoryUserDetailsManager với
+		// user vừa tạo
+	}
+
+	@Bean
+	public SecurityFilterChain fileChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+		
+		http.formLogin(withDefaults()); // Sử dụng form login mặc định của Spring Security (username, password)
+		
+		http.csrf(csrf -> csrf.disable()); // Tắt chức năng CSRF (Cross-Site Request Forgery) để dễ test
+		
+		http.headers(headers -> headers.frameOptions(frame -> frame.disable())); // Tắt chức năng frameOptions, vì H2 Database Console cần frame nên phải tắt frameOptions (x-frame-options: DENY)
+		
+		
+		return http.build();
 	}
 }
