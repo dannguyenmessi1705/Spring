917b0d405c6b33bbb04896c52374c5031091b014
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-26 18:16:11 +0700

    RestAPI: create params and query for route

--- spring-boot-restapi/src/main/java/com/didan/spring_boot_restapi/controller/HelloController.java
+++ spring-boot-restapi/src/main/java/com/didan/spring_boot_restapi/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.didan.spring_boot_restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,11 @@ public class HelloController {
 	@GetMapping(path = "/hello")
 	public Hello hello() {
 		return new Hello("Hello World!");
+	}
+	
+	@GetMapping(path = "/hello2/{id}")
+	public Hello hello2(@RequestParam("name") String name, @PathVariable("id") int id) {
+		return new Hello("Hello " + name + " with id " + id);
 	}
 
 }
