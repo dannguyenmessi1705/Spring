f07d5ae91dd0c13432a239ccfc9b99103d4f125e
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-29 15:18:07 +0700

    SpringSecurity: FilterChain to cusom httpLogin, disable CSRF to allow POST, PUT method

---
+++ spring-boot-restapi/src/main/java/com/didan/spring_boot_restapi/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.didan.spring_boot_restapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static  org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		// Xác thực tất cả các request
+		http.authorizeHttpRequests(req -> req.anyRequest().authenticated());
+		
+		// Sử dụng form login nếu không xác thực được
+		http.httpBasic(withDefaults());
+		
+		// Tắt CSRF cho POST, PUT, DELETE request
+		http.csrf(csrf -> csrf.disable());
+		
+		return http.build();
+	}
+
+}
