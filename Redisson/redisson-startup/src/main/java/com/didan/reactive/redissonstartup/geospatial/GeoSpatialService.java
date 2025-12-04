8ea21151184fb3870fdf3bd193c6e9f692b4db3d
dannd1 <dannd1@viettel.com.vn>
2025-12-04 17:28:14 +0700

    geospatial

--- Redisson/redisson-startup/src/main/java/com/didan/reactive/redissonstartup/geospatial/GeoSpatialService.java
+++ Redisson/redisson-startup/src/main/java/com/didan/reactive/redissonstartup/geospatial/GeoSpatialService.java
@@ -1,6 +1,7 @@
 package com.didan.reactive.redissonstartup.geospatial;
 
 import jakarta.annotation.PostConstruct;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.GeoUnit;
@@ -11,7 +12,6 @@ import org.redisson.api.geo.OptionalGeoSearch;
 import org.redisson.codec.TypedJsonJacksonCodec;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
@@ -24,17 +24,16 @@ public class GeoSpatialService {
   public void geoSpatialI() {
     RGeoReactive<RestaurantDto> geo = redissonReactiveClient.getGeo("restaurants", new TypedJsonJacksonCodec(RestaurantDto.class)); // Sử dụng TypedJsonJacksonCodec để mã hóa/giải mã đối tượng RestaurantDto trong Redis Key "restaurants"
     // Thêm dữ liệu địa lý mẫu
-    Mono<Void> mono = Flux.fromIterable(RestaurantUtils.getRestaurants()) // Lấy danh sách nhà hàng mẫu
+    Flux.fromIterable(RestaurantUtils.getRestaurants()) // Lấy danh sách nhà hàng mẫu
         .flatMap(r -> geo.add(r.getLongitude(), r.getLatitude(), r)) // Thêm từng nhà hàng vào cấu trúc địa lý của Redisson (sử dụng kinh độ, vĩ độ và đối tượng nhà hàng)
         .doOnComplete(() -> log.info("GeoSpatial I executed")) // Ghi log khi hoàn thành việc thêm dữ liệu
-        .then(); // Chuyển đổi sang Mono<Void> để biểu thị hoàn thành
+        .subscribe(); // Thực thi chuỗi các thao tác (Trong Reactive (phản ứng) cần có sự kiện subscribe để bắt đầu thực thi)
 
-    OptionalGeoSearch radius = GeoSearchArgs.from(-96.43243, 32.35553).radius(6, GeoUnit.MILES); // Tạo đối tượng tìm kiếm bán kính với tọa độ và bán kính cụ thể (3 km)
+    OptionalGeoSearch radius = GeoSearchArgs.from(-96.80359, 32.78136).radius(3, GeoUnit.MILES); // Tạo đối tượng tìm kiếm bán kính với tọa độ và bán kính cụ thể (3 km)
     // Thực hiện tìm kiếm các nhà hàng trong bán kính đã chỉ định
-    Mono<Void> result = geo.search(radius)
+    geo.search(radius) // Thực hiện tìm kiếm trong cấu trúc địa lý với điều kiện bán kính đã tạo
+        .flatMapIterable(Function.identity()) // Chuyển đổi kết quả tìm kiếm thành luồng các nhà hàng
         .doOnNext(r -> log.info("Restaurant found: {}", r)) // Ghi log thông tin nhà hàng tìm thấy
-        .then(); // Kết thúc chuỗi phản ứng
-
-    mono.concatWith(result).subscribe(); // Kết hợp hai Mono và đăng ký để thực thi
+        .subscribe(); // Thực thi chuỗi các thao tác tìm kiếm
   }
 }
